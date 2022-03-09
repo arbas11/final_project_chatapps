04a359d88dbb9ddf4ad0c74e0d43a3351b22ae8b
ario baskoro <ariobaskoro@arios-MacBook-Pro-2.local>
2022-03-10 00:26:01 +0700

    react template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import Mainpage from "./pages/Mainpages/Mainpage";
+
+function App() {
+  return (
+    <div className="App">
+      <Mainpage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Mainpages/Mainpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mainpage() {
+  return (
+    <div className='mainpages-container'>
+        <div className='test'> test</div>
+    </div>
+  )
+}
+
+export default Mainpage
