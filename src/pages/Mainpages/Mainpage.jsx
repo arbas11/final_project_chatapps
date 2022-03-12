2132a32d7bcfdb1b8edf51a3789359aa7dbe9469
Cindy Nainggolan <72927218+rosieposiecngl@users.noreply.github.com>
2022-03-12 12:28:05 +0700

    Update Mainpage.jsx

--- src/pages/Mainpages/Mainpage.jsx
+++ src/pages/Mainpages/Mainpage.jsx
@@ -3,13 +3,17 @@ import MainPageHeader from '../../components/main-page-header/MainPageHeader'
 import Chatactive from '../chatlist/Chatlist'
 import Chatroom from '../chattingroom/Chatroom'
 import Contactinfo from '../contact-info/Contactinfo'
+// import Navbar from '../navbar/Navbar'
 
 function Mainpage() {
   return (
+    
     <div className='mainpages-container'>
         <div className='mainpages-components'>
         <MainPageHeader />
         <div className='app-component-container'>
+        <div className='navbar'>
+        </div>
             <Chatactive />
             <Chatroom />
             <Contactinfo />
